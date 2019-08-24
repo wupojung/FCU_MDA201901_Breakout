f58df36638f5620536d64fa5c3b12e3024b07e9b
Po Jung Wu <wupojung@gmail.com>
2019-08-24 23:59:09 +0800

    add Game Framework

--- app/src/main/java/net/wustudio/game/breakout/MainActivity.java
+++ app/src/main/java/net/wustudio/game/breakout/MainActivity.java
@@ -2,6 +2,7 @@ package net.wustudio.game.breakout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //this is an demo , just jump to second Scene
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, InGameActivity.class);
+        startActivity(intent);
     }
 }
